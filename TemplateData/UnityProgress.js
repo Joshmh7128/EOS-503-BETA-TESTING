b173c3c10876af7bc742facae15413748d22021a
Josh <35745910+Joshmh7128@users.noreply.github.com>
2022-05-02 12:59:31 -0400

    Removing cache. testing again

--- TemplateData/UnityProgress.js
+++ TemplateData/UnityProgress.js
@@ -1,24 +1,46 @@
-function UnityProgress(unityInstance, progress) {
+function UnityProgress(unityInstance, progress)
+{
   if (!unityInstance.Module)
     return;
-  if (!unityInstance.logo) {
-    unityInstance.logo = document.createElement("div");
-    unityInstance.logo.className = "logo " + unityInstance.Module.splashScreenStyle;
+
+  if (!unityInstance.logo)
+  {
+    unityInstance.logo = document.getElementById("custom-logo");
+    unityInstance.logo.style.display = "block";
     unityInstance.container.appendChild(unityInstance.logo);
   }
-  if (!unityInstance.progress) {    
-    unityInstance.progress = document.createElement("div");
-    unityInstance.progress.className = "progress " + unityInstance.Module.splashScreenStyle;
-    unityInstance.progress.empty = document.createElement("div");
-    unityInstance.progress.empty.className = "empty";
-    unityInstance.progress.appendChild(unityInstance.progress.empty);
-    unityInstance.progress.full = document.createElement("div");
-    unityInstance.progress.full.className = "full";
-    unityInstance.progress.appendChild(unityInstance.progress.full);
+
+  if (!unityInstance.progress)
+  {
+    unityInstance.progress = document.getElementById("custom-loader");
+    unityInstance.progress.style.display = "block";
     unityInstance.container.appendChild(unityInstance.progress);
   }
-  unityInstance.progress.full.style.width = (100 * progress) + "%";
-  unityInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";
+
+  setLoaderProgressTo(progress);
+
   if (progress == 1)
-    unityInstance.logo.style.display = unityInstance.progress.style.display = "none";
+  {
+    unityInstance.logo.style.display = "none";
+    unityInstance.progress.style.display = "none";
+  }
+}
+
+// value - 0 to 1
+function setLoaderProgressTo(value)
+{
+  const fill = unityInstance.progress.getElementsByClassName("fill")[0];
+  const fillText = unityInstance.progress.getElementsByClassName("label")[0];
+
+  fill.animate(
+    [
+      { width: (value * 100) + "%" }
+    ],
+    {
+      duration: 300,
+      fill: "forwards"
+    }
+  );
+
+  fillText.textContent = (value * 100).toFixed() + "%";
 }
